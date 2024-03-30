435f7e5c6dfee5d4bb8d34730abc295c5acf4552
Josue53 <146614782+Josue53@users.noreply.github.com>
2024-03-30 02:40:11 -0600

    Add files via upload
    Carpeta de Trabajo para el proyecto con algunas clases

---
+++ Fidefy/src/main/java/Clases/Fidefy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Clases;
+
+/**
+ *
+ * @author Grupo_2
+ */
+public class Fidefy {
+
+    public static void main(String[] args) {
+        
+    }
+}
