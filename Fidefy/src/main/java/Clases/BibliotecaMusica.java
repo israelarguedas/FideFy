388a84811112f7918925455f8ece9251b7e7426d
israelarguedas <134347102+israelarguedas@users.noreply.github.com>
2024-03-31 21:18:42 +0000

    Update BibliotecaMusica.java

--- Fidefy/src/main/java/Clases/BibliotecaMusica.java
+++ Fidefy/src/main/java/Clases/BibliotecaMusica.java
@@ -1,13 +1,44 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * @author Grupo_2
+ * @author Isra Arguedas
  */
+
 package Clases;
 
-/**
- *
- * @author Grupo_2
- */
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class BibliotecaMusica {
+    private ArrayList<Cancion> miBiblioteca;
+
+    public BibliotecaMusica() {
+    }
+
+    public BibliotecaMusica(ArrayList<Cancion> miBiblioteca) {
+        this.miBiblioteca = miBiblioteca;
+    }
+
+    public ArrayList<Cancion> getMiBiblioteca() {
+        return miBiblioteca;
+    }
+
+    public void setMiBiblioteca(ArrayList<Cancion> miBiblioteca) {
+        this.miBiblioteca = miBiblioteca;
+    }
+
+    public void agregar_cancion(Cancion NuevaCancion){
+        this.miBiblioteca.add(NuevaCancion);
+        NuevaCancion.setMeGusta(true);
+        JOptionPane.showMessageDialog(null,
+                "La cancion " + NuevaCancion.getTitulo() + " ha sido agregado a la biblioteca");
+    }
+    
+    public void remover_cancion(Cancion NuevaCancion){
+        this.miBiblioteca.remove(NuevaCancion);
+        NuevaCancion.setMeGusta(false);
+        JOptionPane.showMessageDialog(null,
+                "La cancion " + NuevaCancion.getTitulo() + " ha sido removida de la biblioteca");
+    }    
     
 }
+
