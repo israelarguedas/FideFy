c88e2e66b77715bbc9ecf14977f5c09c8e88323e
Dario Monge <100dario@gmail.com>
2024-04-17 21:08:15 -0600

    Creacion de clase Mensaje, sus parametros y metodos basicos

---
+++ Fidefy/src/main/java/Clases/Mensaje.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author 100da
+ */
+public class Mensaje {
+    private String emisor;
+    private String receptor;
+    private int tema;
+    private String contenido;
+    private String fecha;
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String emisor, String receptor, String contenido, String fecha) {
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.contenido = contenido;
+        this.fecha = fecha;
+    }
+
+    public Mensaje(String emisor, int tema, String contenido, String fecha) {
+        this.emisor = emisor;
+        this.tema = tema;
+        this.contenido = contenido;
+        this.fecha = fecha;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public void setEmisor(String emisor) {
+        this.emisor = emisor;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
+
+    public void setReceptor(String receptor) {
+        this.receptor = receptor;
+    }
+
+    public int getTema() {
+        return tema;
+    }
+
+    public void setTema(int tema) {
+        this.tema = tema;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
